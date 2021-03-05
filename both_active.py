98c955542ffa7eb27699af495a7e104df1d724b1
unknown <lucmcgrady@gmail.com>
2021-03-05 03:18:33 +0000

    Added: both_active

--- both_active.py
+++ both_active.py
@@ -1,4 +1,6 @@
 from lib import discordjson
+from collections import Counter
+
 # import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
@@ -6,18 +8,40 @@ if __name__ == '__main__':
 
     startday = messages[0].get_datetime().date()
 
-    total_days = (messages[-1].get_datetime().date() - startday).days
+    message_days = {}  # Messages seperated into days
+
+    lastday = -1
+    for message in messages:
+        currentday = (message.get_datetime().date() - startday).days
+        if currentday != lastday:
+            message_days[currentday] = []
+        message_days[currentday].append(message)
+        lastday = currentday
 
     messagesent = []
-
-    messagedates = [(message.get_datetime().date() - startday).days for message in messages]
+    total_days = max(message_days)
 
     for day in range(total_days):
-        messagesent.append(day in messagedates)
+        if day not in message_days:
+            messagesent.append(())
+            continue
 
-    # print(messagesent)
-    print(
-        """Days since first message sent = {days}
-Days where messages were sent = {}/{days}
-Days where messages were not sent = {}/{days}""".format(
-            sum(messagesent), total_days - sum(messagesent), days=total_days))
+        todays_authors = []
+        for message in message_days[day]:
+            author = message.values["author"]["name"]
+            if author not in todays_authors:
+                todays_authors.append(author)
+
+        messagesent.append(tuple(sorted(todays_authors)))
+
+    outstring = "Who messaged on how many days:\n"  # The string that will be printed
+
+    for authors, count in Counter(messagesent).items():
+        if len(authors) == 0:
+            outstring += "Nobody"
+        else:
+            outstring += ", ".join(authors)  # This is probably bad practice but to make it worse I wont explain why
+
+        outstring += ": %r\n" % count
+
+    print(outstring)
